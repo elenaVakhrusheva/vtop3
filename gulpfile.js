45ee92e9e704d99448f9bdb823fc1650cca90037
«Ваше <elena.vakhrusheva92@yandex.ru>
2023-03-09 15:46:35 +0400

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,31 @@
-const gulp = require('gulp'),
-      concatCSS = require('gulp-concat-css'),
-      less = require('gulp-less');
-      minifyCSS = require('gulp-minify-css'), 
-      autoprefixer = require('gulp-autoprefixer');
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    less        = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concatCss 	= require('gulp-concat-css');
 
-gulp.task('default', function(){
-  return gulp.src('src/*.html')
-    .pipe(gulp.dest('dist/html'))
+gulp.task('less', function(done) {
+    gulp.src("scr/css/*.less")
+        .pipe(less())
+        .pipe(gulp.dest("dist/css"))
+        .pipe(concatCss('main.css'))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(browserSync.stream());
+    done();
 });
 
-gulp.task('less', function () {
-  gulp.src('src/css/*.less')
-    .pipe(less())
-    .pipe(concatCSS('css/main.css'))
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('dist/'));
+gulp.task('serve', function(done) { 
+    browserSync.init({
+        server: "./dist",
+        index: "html/index.html" 
+    });
+    gulp.watch("src/css/*.less", gulp.series('less')); 
+    gulp.watch("dist/html/index.html").on('change', () => {
+      browserSync.reload();
+      done();
+    });
+
+    done();
 });
 
- 
+gulp.task('default', gulp.series('less', 'serve'));
