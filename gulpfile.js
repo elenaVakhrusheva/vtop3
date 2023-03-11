bde4ddfe6f001ff7cd77e69e10c6a43112cf6dc3
«Ваше <elena.vakhrusheva92@yandex.ru>
2023-03-11 21:48:36 +0400

    I made the layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,53 @@
-var gulp        = require('gulp'),
-    browserSync = require('browser-sync').create(),
-    less        = require('gulp-less'),
+var gulp         = require('gulp'),
+    browserSync  = require('browser-sync').create(),
+    less         = require('gulp-less'),
     autoprefixer = require('gulp-autoprefixer'),
-    concatCss 	= require('gulp-concat-css');
+    concatCss 	 = require('gulp-concat-css'),
+		del          = require('del');
 
-gulp.task('less', function(done) {
-    gulp.src("src/css/*.less")
-        .pipe(less())
-        .pipe(autoprefixer('last 2 versions'))
-        .pipe(gulp.dest("dist/css"))
-        .pipe(concatCss('main.css'))
-        .pipe(browserSync.stream());
-    done();
+gulp.task('clean', async function() {
+	return del.sync(['dist']);
 });
 
-gulp.task('serve', function(done) { 
-    browserSync.init({
-        server: "./dist",
-        index: "html/index.html" 
-    });
-    gulp.watch("src/css/*.less", gulp.series('less')); 
-    gulp.watch("dist/html/index.html").on('change', () => {
-      browserSync.reload();
-      done();
-    });
-
-    done();
+gulp.task('less', function() {
+	return gulp.src("src/css/*.less")
+		.pipe(less())
+		.pipe(autoprefixer('last 2 versions'))
+		.pipe(concatCss('main.css'))
+		.pipe(gulp.dest("dist/css"))
+		.pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.series('less', 'serve'));
+
+gulp.task('html', function() {
+	return gulp.src("src/index.html")
+		.pipe(gulp.dest("dist"))
+		.pipe(browserSync.stream());
+});
+
+
+gulp.task('img', function() {
+	return gulp.src("src/img/*.*")
+		.pipe(gulp.dest("dist/img"))
+		.pipe(browserSync.stream());
+});
+
+
+gulp.task('serve', function() {
+	browserSync.init({
+		server: "./dist",
+		index: "index.html"
+	});
+	gulp.watch("src/css/*.less", gulp.series('less'));
+	gulp.watch("src/img/*.*", gulp.series('img'));
+	gulp.watch("src/index.html", gulp.series('html'));
+	gulp.watch("dist/index.html").on('change', () => {
+		browserSync.reload();
+	});
+});
+
+
+gulp.task('default', gulp.series(
+	'clean',
+	gulp.parallel('less', 'img', 'html', 'serve')
+));
