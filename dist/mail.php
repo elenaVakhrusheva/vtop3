7854289b44c8a00ab29450512ffbbd44889f9c84
«Ваше <elena.vakhrusheva92@yandex.ru>
2023-03-17 11:13:39 +0400

    update style, add php pages

--- dist/mail.php
+++ dist/mail.php
@@ -1,18 +1,20 @@
-<?php
-  $name=$_POST['name'];
-  $lastName=$_POST['lastName'];
-  $national=$_POST['national'];
-  $email=$_POST['email'];
-   
-
-  $to = "elena.vakhrusheva92@yandex.ru";
-  $subject = "Заявка с сайта";
-  $message = "
-  Имя пользователя: ".htmlspecialchars($name)."</br>
-  Email: ".htmlspecialchars($email)." </br>
-  Сообщение: ".htmlspecialchars($lastName).htmlspecialchars($national)
-
-  $headers = "From: Test task \r\nContent-type:text/html; charset=UTF-8 \r\n";
-  mail($to, $subject, $message, $headers);
-  header("Location: thanks.html")
+<?
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' && !empty($_POST['name'])) {
+    $message = 'Name: ' . $_POST['name'] . ' ';
+    $message = 'lastName: ' . $_POST['lastName'] . ' ';
+    $message .= 'Телефон: ' . $_POST['phone'] . ' ';
+    if(!empty($_POST['text'])) {
+        $message .= 'Текст: ' . $_POST['text'] . ' ';
+    }
+    $mailTo = "elena.vakhrusheva92@yandex.ru"; // Ваш e-mail
+    $subject = "Письмо с сайта"; // Тема сообщения
+    $headers= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8\r\n";
+    $headers .= "From: info@site.ru <info@site.ru>\r\n";
+    if(mail($mailTo, $subject, $message, $headers)) {
+        echo "Спасибо, ".$_POST['name'].", мы свяжемся с вами в самое ближайшее время!"; 
+    } else {
+        echo "Сообщение не отправлено!"; 
+    }
+}
 ?>
